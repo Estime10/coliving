21dc8d4be5b86218c81de34f274e7ab855a02824
Estime Vangu <107044375+Estime10@users.noreply.github.com>
2024-02-19 14:57:13 +0100

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+// NAVIGATION
+
+export const NAV_LINKS = [
+  { href: '/home', key: 'home', label: 'Home' },
+  {
+    href: '/hotspots',
+    key: 'hotspots',
+    label: 'Hot Spots',
+  },
+  {
+    href: '/properties',
+    key: 'properties',
+    label: 'Properties',
+  },
+  { href: '/contact', key: 'contact', label: 'Contact' },
+];
+
+// SELECT OPTIONS
+
+export const LOCATIONS = [
+  { key: 'all', value: 'all', label: 'All Locations' },
+  { key: 'Brussels', value: 'Brussels', label: 'Brussels' },
+  { key: 'Antwerp', value: 'Antwerp', label: 'Antwerp' },
+  { key: 'Ghent', value: 'Ghent', label: 'Ghent' },
+  { key: 'Bruges', value: 'Bruges', label: 'Bruges' },
+];
